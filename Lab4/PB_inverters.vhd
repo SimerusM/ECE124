f2bf1deb89f0b73f3b6ca795a41bfc0a3472b307
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-03-29 13:56:15 -0400

    Update PB_inverters.vhd

--- Lab4/PB_inverters.vhd
+++ Lab4/PB_inverters.vhd
@@ -2,20 +2,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-
-entity PB_inverters is port (
-	rst_n				: in	std_logic;
-	rst				: out std_logic;
- 	pb_n_filtered	: in  std_logic_vector (3 downto 0);
-	pb					: out	std_logic_vector(3 downto 0)							 
-	); 
+-- Declaration of the entity PB_inverters. It serves as the interface of the module,
+-- specifying inputs and outputs.
+entity PB_inverters is
+    port (
+        rst_n           : in  std_logic;                -- Active low reset input
+        rst             : out std_logic;                -- Active high reset output
+        pb_n_filtered   : in  std_logic_vector (3 downto 0); -- Input vector, active low pushbuttons
+        pb              : out std_logic_vector(3 downto 0)   -- Output vector, active high pushbuttons
+    ); 
 end PB_inverters;
 
+-- Architecture ckt of PB_inverters begins. This section defines the internal logic of the entity.
 architecture ckt of PB_inverters is
-
 begin
-rst <= NOT(rst_n);
-pb <= NOT(pb_n_filtered);
+    -- Invert the active low reset input to generate an active high reset output
+    rst <= NOT(rst_n);
+    
+    -- Invert the active low pushbutton inputs to generate active high pushbutton outputs
+    pb <= NOT(pb_n_filtered);
 
-
-end ckt;
+end ckt; -- End of architecture
